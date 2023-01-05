72d7f627cbda39fdbbb52a852a87b2083c9203ae
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2023-01-05 12:29:44 -0600

    remove comment from controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,8 @@
 class MessagesController < ApplicationController
   def index
-    # @message = Message..order("RANDOM()").first
-    # @message = Message.order("RAND()").limit(1)
+   
     @message = Message.limit(2).order('RANDOM()')
-    # json_response(@message)
+  
     render json: @message, status: :ok
   end
 end
